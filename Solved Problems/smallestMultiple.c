456740ffe03ff6e17d626601f86377f1f3f1048b
Shn Kerby Dolamos <140078330+Shinkrbs@users.noreply.github.com>
2023-10-29 15:24:32 +0800

    Update smallestMultiple.c

--- Solved Problems/smallestMultiple.c
+++ Solved Problems/smallestMultiple.c
@@ -1,18 +1,23 @@
 #include <stdio.h>
 
-int max_power(int prime, int limit) {
+int max_power(int prime, int limit) 
+{
     int power = 1;
-    while (prime * power <= limit) {
+    while (prime * power <= limit)
+    {
         power *= prime;
     }
+    
     return power;
 }
 
-int main() {
+int main() 
+{
     int limit = 20;
     long long lcm = 1;
 
-    for (int i = 1; i <= limit; i++) {
+    for (int i = 1; i <= limit; i++) 
+    {
         lcm *= max_power(prime, limit);
     }
 
